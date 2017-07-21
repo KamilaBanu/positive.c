594fff9b1bdc0fd2fa8a4648a6465867aaa71c8b
KamilaBanu <30333760+KamilaBanu@users.noreply.github.com>
2017-07-21 16:02:27 +0530

    positive.c

---
+++ positive.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf("enter the no:");
+scanf("%d",&a);
+if(a>0)
+{
+printf("number is positive");
+}
+else
+if(a<0)
+{
+printf("number is negative");
+}
+else
+{
+printf("number is zero");
+}
+}
