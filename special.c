9bbd4669d405336963aae6d281009d8b9fceeb0e
KamilaBanu <30333760+KamilaBanu@users.noreply.github.com>
2017-10-10 16:21:49 +0530

    special.c

---
+++ special.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    char    str[100];
+   int countSp;
+    int counter;
+    countSp=0;
+  printf("Enter a string: ");
+    gets(str); 
+    for(counter=0;str[counter]!=NULL;counter++)
+    {
+        if(str[counter]==' ')
+            countSp++;
+    }
+printf("%d",Sp);
+return 0;
+}
+ 
